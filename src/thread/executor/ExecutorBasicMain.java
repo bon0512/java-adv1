ea1822562ba5e9a82f72431584f2420da5bfce2e
bon0512 <nonstop00450@gmail.com>
2025-07-08 21:25:16 +0900

    executor 기본 배우기

--- src/thread/executor/ExecutorBasicMain.java
+++ src/thread/executor/ExecutorBasicMain.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import static thread.executor.ExecutorUtils.printState;
 import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
 
 public class ExecutorBasicMain {
     public static void main(String[] args) {
@@ -14,5 +15,19 @@ public class ExecutorBasicMain {
         ExecutorService es = new ThreadPoolExecutor(2,2,0, TimeUnit.MILLISECONDS,new LinkedBlockingDeque<>());
         log("== 초기상태 ==");
         printState(es);
+        es.execute(new RunnableTask("taskA"));
+        es.execute(new RunnableTask("taskB"));
+        es.execute(new RunnableTask("taskC"));
+        es.execute(new RunnableTask("taskD"));
+        log("== 작업 수행 중 ==");
+        printState(es);
+        sleep(3000);
+        log("작업 수행 완료");
+        printState(es);
+
+        es.close();
+        log("== shutdown 완료");
+        printState(es);
+
     }
 }
